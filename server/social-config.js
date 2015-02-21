89e2c3cb4281965ad8b257f307b9830babad7e79
Justin Krup <mazlix@gmail.com>
2015-02-21 14:02:49 -0500

    removed my API keys
    And reset the secret key on FB =/

--- server/social-config.js
+++ server/social-config.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
  
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '1580436792199787',
-    secret: '2f92a0ce55c167c1c6f5442036ce8d2e'
+    appId: '*************',
+    secret: '********************************'
 });
